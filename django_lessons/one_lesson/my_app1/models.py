299a3b44cc186f9cdb6d2a54b36a92a23b873120
AndreyMakarov-lol <64976956+AndreyMakarov-lol@users.noreply.github.com>
2023-08-06 15:11:49 +0300

    last_lesson

--- django_lessons/one_lesson/my_app1/models.py
+++ django_lessons/one_lesson/my_app1/models.py
@@ -7,7 +7,7 @@ class Worker(models.Model):
     name = models.CharField(max_length=20, blank=False)  # Создаём столбец name со
     # строковым типом данных (CharField), максимальной длинной 20 символов (max_length=20), запретом на пустое поле(
     # blank=False)
-    second_name: CharField = models.CharField(max_length=35, blank=False)  # Создаём столбец second_name аналогично предидущему
+    second_name = models.CharField(max_length=35, blank=False)  # Создаём столбец second_name аналогично предидущему
     salary = models.IntegerField(default=0)  # Создаём столбец salary с числовым типом данных
     # с начальным значением 0
     def __str__(self):
